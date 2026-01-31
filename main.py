dee629acdc73e0716a4590e27ddd84ecefd80cda
DJ Universe <104515745+ShonShafir@users.noreply.github.com>
2026-01-31 21:08:30 +0200

    Reorder print statements and function calls

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import extract_artists
 import check_new_releases
 
 def main():
-    print("Extracting artist IDs...")
-    extract_artists.extract_artist_ids()
-    #print("Checking for new releases...")
-    #check_new_releases.check_new_releases()
-    #print("Done!")
+    #print("Extracting artist IDs...")
+    #extract_artists.extract_artist_ids()
+    print("Checking for new releases...")
+    check_new_releases.check_new_releases()
+    print("Done!")
 
 if __name__ == '__main__':
     main()
