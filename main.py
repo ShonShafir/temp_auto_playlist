f2e1e75d5f864bd07dd330f2f618d34f3a07eacf
DJ Universe <104515745+ShonShafir@users.noreply.github.com>
2026-02-05 23:05:33 +0200

    Toggle extraction and release checking in main

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import extract_artists
 import check_new_releases
 
 def main():
-    #print("Extracting artist IDs...")
-    #extract_artists.extract_artist_ids()
-    print("Checking for new releases...")
-    check_new_releases.check_new_releases()
+    print("Extracting artist IDs...")
+    extract_artists.extract_artist_ids()
+    #print("Checking for new releases...")
+    #check_new_releases.check_new_releases()
     print("Done!")
 
 if __name__ == '__main__':
