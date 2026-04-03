10166e4069ead1f9459d5e552438a36cf0cb4cbb
DJ Universe <104515745+ShonShafir@users.noreply.github.com>
2026-04-04 01:13:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ PLAYLIST_URLS = [
     'https://open.spotify.com/playlist/2q1wEyWJKw65EIiTsZmVNZ?si=3843bdb006034e4d'
 ]
 
-TARGET_PLAYLIST_ID = '2Z5YAQGFSWgtuWo0LlbCGF'
+TARGET_PLAYLIST_ID = '7lzl4v57XbDAaRmRFpz2V9'
 
 ARTISTS_FILE = 'artists_id.txt'
 ADDED_TRACKS_FILE = 'added_tracks.txt'
